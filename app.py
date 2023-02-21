aee9d0e6b364999400822760d83aec455298530d
NextGen20 <amitbachar2@gmail.com>
2023-02-21 13:41:26 +0200

    arg

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
+import sys
 app = Flask(__name__)
-
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def Hello_name():
+    return f'hello {sys.argv[1]}'
 
-if __name__ == '__main__':
-    app.run()
+if __name__=='__main__':
+    app.run(host="0.0.0.0", port=80)
