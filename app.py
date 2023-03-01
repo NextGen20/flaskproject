c08dcd92571ff6ab92bccc2248612df2cbb63a16
NextGen20 <71230554+NextGen20@users.noreply.github.com>
2023-03-01 22:45:42 +0200

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import sys
 app = Flask(__name__)
 @app.route('/')
 def Hello_name():
-    return f'hello {sys.argv[1]}'
+    return f'hello {sys.argv[0]}'
 
 if __name__=='__main__':
     app.run(host="0.0.0.0", port=5000)
