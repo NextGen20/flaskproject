335d7b6f4fd053063333909f8c1a14b5d2dc293a
NextGen20 <71230554+NextGen20@users.noreply.github.com>
2023-04-09 14:40:08 +0300

    project2

--- app.py
+++ app.py
@@ -1,24 +1,45 @@
-from flask import Flask
 import os.path
 from flask import Flask, request, redirect
 from flask.templating import render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, migrate
-import sys
 import subprocess
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///my_site.db'
+# ALLOWED_EXTENSIONS = ["png", "jpeg", "jpg", "gif"]
+# def allowed_file(filename):
+#      return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 db = SQLAlchemy(app)
+
 migrate = Migrate(app, db)
 
 class Profile(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String(20), unique=False, nullable=False)
     last_name = db.Column(db.String(20), unique=False, nullable=False)
+
     def __str__(self):
-        return f"Name:{self.first_name}, last:{self.last_name}"
-    
+        return f"Name:{self.first_name}, Last:{self.last_name}"
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    if request.method == "POST":
+        first_name = request.form.get("fname")
+        last_name = request.form.get("lname")
+        p = Profile(first_name=first_name, last_name=last_name)
+        db.session.add(p)
+        db.session.commit()
+        return redirect('/homepage')
+        # return f"{first_name} {last_name} "
+    return render_template("signup.html")
+
+@app.route("/homepage")
+def homepage():
+    users_data = Profile.query.all()
+    return render_template("homepage.html", users_data=users_data)
+
 @app.route("/docker", methods=["GET", "POST"])
 def docker():
     if request.method == "POST":
@@ -27,21 +48,14 @@ def docker():
         subprocess.run(['git', 'init'])
         subprocess.run(['git', 'add', '.'])
         subprocess.run(['git', 'commit', '-m', f"Pushing {image_name} Docker image to GitHub"])
-        subprocess.run(['git', 'remote', 'add', 'origin', 'https://github.com/NextGen20/flaskproject.git'])
-        subprocess.run(['git', 'push', '-u', 'origin', 'main'])
+        subprocess.run(['git', 'remote', 'add', 'origin', 'https://github.com/<username>/<repo_name>.git'])
+        subprocess.run(['git', 'push', '-u', 'origin', 'master'])
         return f"Docker image {image_name} created and pushed to GitHub"
     return render_template("docker.html")
 
-@app.route("/homepage")
-def homepage():
-    users_data = Profile.query.all()
-    return render_template("homepage.html", users_data=users_data)
+    
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
-
-@app.route('/')
-def Hello_name():
-    return f'hello {sys.argv[1]}'
-
-if __name__=='__main__':
-    app.run(host="0.0.0.0", port=5000)
